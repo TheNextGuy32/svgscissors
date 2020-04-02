e0ea898bba95b7653e6ffe92d4bdc8cac2dff5ee
Oliver Barnum <oliverbarnum32@gmail.com>
2020-04-02 06:29:01 -0700

    Rename project

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "scgScissors",
-    version = "0.0.1",
+    name = "svgScissors",
+    version = "0.0.2",
     author = "Oliver Barnum",
     author_email = "oliverbarnum32@gmail.com",
     description = "Manipulate svg xml to automate data insertion.",
     url = "https://github.com/TheNextGuy32/svgScissors",
     install_requires=["asyncio", "ensure", "svgutils", "wand", "mpmath"],
-    download_url="https://github.com/TheNextGuy32/svgScissors/archive/0.0.1.tar.gz",
+    download_url="https://github.com/TheNextGuy32/svgScissors/archive/0.0.2.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
